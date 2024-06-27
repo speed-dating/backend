a8b98526b694b3d00f695b2391f8d2fb3af9bd2d
soonyubi <soonyubi1@naver.com>
2024-06-27 23:14:28 +0900

    init projcect

---
+++ src/main/kotlin/com/backend/speed_dating/SpeedDatingApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.speed_dating
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpeedDatingApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpeedDatingApplication>(*args)
+}
